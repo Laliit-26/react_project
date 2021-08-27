5e19ba8d8a5e6531c4ec72a4f820c8aa17a661eb
lalit <tiwarilalit2601@gmail.com>
2021-08-27 15:00:18 +0530

    alert added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { useState, useEffect } from 'react';
+import Axios from './components/Axios';
+import Alerts from './components/Alerts';
+
 
 function App() {
+  const [mode, setmode] = useState('light');
+  const [alert, setalert] = useState(null);
+
+    const showalert = (message, type) => {
+      setalert({
+        message: message,
+        type: type,
+      })
+      setTimeout(() => {
+        setalert(null);
+      }, 1500);
+    }
+
+
+  if (mode === "dark")
+  {
+    document.body.style.backgroundColor = '#042743';
+   
+   }
+  const togglemode = () => {
+    if (mode==='light')
+    {
+      setmode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showalert('dark mode has enabled', 'success');
+    }
+    else {
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      showalert('light mode has enabled', 'success')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar title='TextUtils' mode={mode} toggle={togglemode} />
+      <Alerts alert={alert}/>
+       <TextForm heading='Enter the text to analyze below' mode={mode} showalert={showalert} />
+{/* <Axios/> */}
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Alerts.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Alerts(props) {
+    // making first letter of type uppercase
+    const cap = (word) => {
+        let lower = word.toLowerCase();
+        return lower[0].toUpperCase() + lower.slice(1);
+    }
+  
+    return (
+        // work only if props.alert is not null
+        props.alert && (
+            <div
+                className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+                role='alert'
+            >
+                <strong>{cap(props.alert.type)}</strong>: {props.alert.message}
+                {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+            </div>
+    )
+    )
+   
+
+}
+
+export default Alerts
